ade95a9038bad5c2e5e6dbe5eb2978110b505d42
Charles Thomas <charles@charlesmthomas.com>
2017-06-14 12:06:54 -0400

    Create VideoList functional component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCQBZkH-gjGJ15lUmxZnonD0ejxyXpQeo8';
 
 class App extends Component {
@@ -12,7 +13,7 @@ class App extends Component {
             videos: [] 
         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (vidoes) => {
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             this.setState({ videos });
         });
     }
@@ -21,6 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
