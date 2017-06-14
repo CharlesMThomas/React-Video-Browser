36a03280683802b3faa22aac4545ec281dbe6330
Charles Thomas <charles@charlesmthomas.com>
2017-06-14 09:23:52 -0400

    Refactor App from functional component to class
    Set the constructor function and make initial API call to the Yelp
    API. Store the API results in the App's state object.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCQBZkH-gjGJ15lUmxZnonD0ejxyXpQeo8';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [] 
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (vidoes) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
